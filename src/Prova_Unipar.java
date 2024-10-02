f7876ac94238274498580097f27affef0082e0b8
Willi-anD <willian270597@edu.unipar.br>
2024-10-02 20:07:28 -0300

    inicial

---
+++ src/Prova_Unipar.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void Prova_Unipar(String[] args) {
+        scanner sc = new scanner (System.in){
+
+            int = 0{
+
+                System.out.println("Digite quantos filmes deseja avaliar");
+
+
+            }
+        }
+    }
+}
